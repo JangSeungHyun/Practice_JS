388410870778a97ec0ee904358f297d25bfb42b0
JangSeungHyun <jang1751@snu.ac.kr>
2020-08-29 18:14:09 +0900

    if_practice

---
+++ Switch_practice.js
@@ -0,0 +1,28 @@
+var select = document.querySelector('select');
+var para = document.querySelector('p');
+
+select.addEventListener('change', setWeather);
+
+function setWeather(){
+    var choice = select.value;
+
+    switch (choice){
+        case 'sunny':
+            para.textContent = 'It is nice and sunny'
+            break;
+        
+        case 'rainy':
+            para.textContent = 'Rain is falling outside;';
+            break;
+        
+        case 'swnowing':
+            para.textContent = 'The snow is coming down';
+            break;
+        
+        case 'overcast':
+            para.textContent = 'It isn\'t raining';
+            break;
+        default:
+            para.textContent = '';
+    }
+}
